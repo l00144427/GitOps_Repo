b355ff7212099bca59727d1a7ee10d7d2f208091
Emmet <l00144427@student.lyit.ie>
2020-03-19 19:15:01 +0000

    Some changes

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 # Terraform state will be stored in S3
 terraform {
   backend "s3" {
-    bucket = "terraform-bucket-l00144425"
-    key    = "terraform.tfstate"
+    bucket = "terraform-bucket-l00144427"
+    #key    = "terraform.tfstate"
     region = "us-east-1"
     access_key = "AKIAZHKULKPYHQ3AGXXU"
     secret_key = "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"
