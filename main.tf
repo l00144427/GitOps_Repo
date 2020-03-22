e22ac10b5b7842610561ca16e1b40dda22be5b43
Emmet <l00144427@student.lyit.ie>
2020-03-22 14:18:52 +0000

    Small changes

--- main.tf
+++ main.tf
@@ -29,7 +29,7 @@ resource "aws_instance" "default" {
   
   connection {
     private_key = "var.key_name"
-  #   host        = "default.hostname"
+  #  dfd  host        = "default.hostname"
     user        = "ubuntu"
   }
 }
