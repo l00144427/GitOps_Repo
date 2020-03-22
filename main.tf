0ffc3bfc0a8d555a724de80f0df23fde2c94c9a0
Emmet <l00144427@student.lyit.ie>
2020-03-22 16:49:55 +0000

    Small changes

--- main.tf
+++ main.tf
@@ -48,6 +48,12 @@ resource "aws_security_group" "default" {
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["109.78.37.103/32"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -56,6 +62,12 @@ resource "aws_security_group" "default" {
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["109.78.37.103/32"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
