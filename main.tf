93a4ce8e966b73eab9b3a708752e6a9539747fb2
Emmet <l00144427@student.lyit.ie>
2020-03-25 13:13:57 +0000

    Changed the inboud rules

--- main.tf
+++ main.tf
@@ -71,6 +71,13 @@ resource "aws_security_group" "default" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 9000
+    to_port     = 9000
+    protocol    = "tcp"
+    cidr_blocks = ["109.78.37.103/32"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 65535
