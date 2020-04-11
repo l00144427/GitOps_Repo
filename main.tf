6ede48acbfc4068a51a2cfac40790c612a462961
Emmet <l00144427@student.lyit.ie>
2020-04-11 18:27:55 +0100

    Fixing the Outbound rules

--- main.tf
+++ main.tf
@@ -72,4 +72,11 @@ resource "aws_security_group" "default" {
     protocol    = "tcp"
     security_groups = ["sg-095576bc1688d6b7a"]
   }
+
+  egress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
