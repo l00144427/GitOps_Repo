f57042672b28829302228eb445c5136d17787131
Emmet <l00144427@student.lyit.ie>
2020-04-11 18:03:59 +0100

    Making the Docker Sonarqube run command only execute if it hasn't already been run

--- main.tf
+++ main.tf
@@ -69,7 +69,7 @@ resource "aws_security_group" "default" {
   egress {
     from_port   = 0
     to_port     = 65535
-    protocol    = "All"
+    protocol    = "all"
     security_groups = ["sg-095576bc1688d6b7a"]
   }
 }
